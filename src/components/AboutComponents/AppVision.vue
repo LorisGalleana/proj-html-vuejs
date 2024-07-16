674120a71fbff090b1515e4ec469d677893ab2c3
LorisGalleana <loris.galleana@gmail.com>
2024-07-16 18:15:44 +0200

    feat: app vision complete

--- src/components/AboutComponents/AppVision.vue
+++ src/components/AboutComponents/AppVision.vue
@@ -1,16 +1,59 @@
 <script>
-
-
-
 export default {
     name: "AppVision"
 }
 </script>
 
 <template>
-    
+    <div class="container-fluid color-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-white text-center py-5 fs-1">
+                    Our Visions
+                </div>
+            </div>
+            <div class="row text-white text-center">
+                <div class="col-3">
+                    <div class=""><i class="icon  fa-solid fa-database" style="color: #ffffff;"></i></div>
+                    <p class="fs-6 fw-semibold">Virtual Document </p>
+                    <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
+                </div>
+                <div class="col-3">
+                    <div><i class="icon fa-solid fa-money-bill-1-wave"></i></div>
+                    <p class="fs-6 fw-semibold">Flexible Payments</p> 
+                    <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
+                </div>
+                <div class="col-3">
+                    <div><i class="icon fa-solid fa-gauge-high"></i></div>
+                    <p class="fs-6 fw-semibold">SEO Tools</p>
+                    <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
+                </div>
+                <div class="col-3">
+                    <div><i class="icon fa-regular fa-gem"></i></div>
+                    <p class="fs-6 fw-semibold">UI Design</p>
+                    <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
+@use "../../styles/partials/variables" as *;
 
+.color-bg {
+    background: $green-gradient;
+    height: 500px;
+}
+.mini-font {
+    font-size: 0.7rem;
+}
+.icon {
+    font-size: 3rem;
+    display: inline-block;
+    border: solid 1px white;
+    border-radius: 50%;
+    padding: 1.2rem;
+    margin-bottom: 20px ;
+}
 </style>
