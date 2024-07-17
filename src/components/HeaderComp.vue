afd2dc34bd5c962b34185cac84491d2c4a6099a4
LorisGalleana <loris.galleana@gmail.com>
2024-07-17 18:32:49 +0200

    push di fine serata

--- src/components/HeaderComp.vue
+++ src/components/HeaderComp.vue
@@ -1,31 +1,140 @@
 <script>
 export default {
-    name: "HeaderComp"
+    name: "HeaderComp",
+    data() {
+        return {
+            routePage: [
+                {
+                    "name": "home",
+                    "available": true
+                },
+                {
+                    "name": "about",
+                    "available": true
+                },
+                {
+                    "name": "services",
+                    "available": false
+                },
+                {
+                    "name": "showcase",
+                    "available": false
+                },
+                {
+                    "name": "blog",
+                    "available": false
+                },
+                {
+                    "name": "contact",
+                    "available": true
+                }
+            ]
+        }
+    }
 }
 
 </script>
 
 <template>
-    <h1>This is a header</h1>
-    <nav>
-        <ul>
-            <li>
-                <router-link :to="{ name: 'home' }" class="nav-link">
-                    Home
-                </router-link>
-            </li>
-            <li>
-                <router-link :to="{ name: 'about' }" class="nav-link">
-                    About Us
-                </router-link>
-            </li>
-            <li>
-                <router-link :to="{ name: 'contact' }" class="nav-link">
-                    Contacts
-                </router-link>
-            </li>
-        </ul>
-    </nav>
+    <div class="container-fluid black">
+        <div class="container upper-container">
+            <div class="contact">
+                <i class="fa-solid fa-phone"></i>
+                <div class="phone-number">
+                    <div>(001)</div> 
+                    <div>88451234</div>
+                    <div>88455438</div>
+                </div>
+            </div>
+            <div class="social">
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-pinterest-p"></i>
+            </div>
+        </div>
+    </div>
+    <div class="container-fluid white">
+        <div class="container d-flex bottom-container">
+            <div class="logo">
+                <img src="/public/img/cropped-Group-39-2x.png" alt="">
+            </div>
+            <nav>
+                <ul class="d-flex router-list">
+                    <li v-for="(route,index) in routePage" :key="index">
+                        <router-link :to="{ name: route.available ? route.name : 'NotFound' }" class="nav-link">
+                            {{route.name.toUpperCase()}}
+                        </router-link>
+                    </li>
+                    
+                </ul>
+            </nav>
+            <div class="purchase-search">
+                <a href="#" class="btn btn-color ">PURCHASE</a>
+                <button class="search"><i class="fa-solid fa-magnifying-glass"></i></button>
+            </div>
+        </div>
+    </div>
+    
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+    .btn-color {
+        background: $green-gradient ;
+        color: white;
+        border-radius: 24px;
+        font-size: 0.7rem;
+        padding: 12px 30px;
+    }
+    .router-list {
+        list-style: none;
+        gap: 30px;
+        li {
+            a {
+                font-size: 0.875rem;
+            }
+        }
+    }
+    .white {
+        height: 100px;
+    }
+    .bottom-container{
+        height: 100%;
+        align-items: center;
+        justify-content: space-between;
+    }
+    .search {
+        background-color: transparent;
+        border: none;
+        color: $green-text;
+        margin-left: 30px;
+    }
+    .logo {
+        transform: scale(0.8);
+    }
+    .black {
+        background-color: #3d3d3d;
+        height: 40px;
+    }
+    .upper-container {
+        display: flex;
+        height: 100%;
+        justify-content: space-between;
+        align-items: center;
+        color: white;
+    }
+    .contact {
+        display: flex;
+        align-items: center;
+    }
+    .social {
+        display: flex;
+        align-items: center;
+        gap: 20px;
+    }
+    .phone-number {
+        display: flex;
+        gap: 10px;
+        margin-left: 10px;
+    }
+</style>
