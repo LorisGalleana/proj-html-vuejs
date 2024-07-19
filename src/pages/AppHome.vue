8c3982b0c8f67ed05efe6c3f4d42689978514d44
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-19 16:56:05 +0200

    Feat: add scroll up button

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -7,6 +7,7 @@ import AppFeedback from '../components/HomeComponents/AppFeedback.vue'
 import AppPricing from '../components/HomeComponents/AppPricing.vue'
 import AppCustomer from '../components/HomeComponents/AppCustomer.vue'
 import AppNews from '../components/HomeComponents/AppNews.vue' 
+import { ref, onMounted, onUnmounted } from 'vue';
 
 export default {
     name: "AppHome",
@@ -19,11 +20,31 @@ export default {
         AppPricing,
         AppCustomer,
         AppNews,
-    }
+    },
+    setup() {
+    const showButton = ref(false);
+
+    const handleScroll = () => {
+      showButton.value = window.scrollY > 500; // Mostra il pulsante dopo 500px di scroll
+    };
+
+    onMounted(() => {
+      window.addEventListener('scroll', handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener('scroll', handleScroll);
+    });
+
+    return {
+      showButton
+    };
+  }
 }
 </script>
 
 <template>
+    <a href="#" class="position-fixed scroll-button" v-if="showButton">&uparrow;</a>
    <AppJumbo />
     <AppFeatures />
     <AppMore />
@@ -35,5 +56,16 @@ export default {
 </template>
 
 <style scoped>
-
+a{
+    text-align: center;
+    bottom: 40px;
+    right: 40px;
+    z-index: 3000;
+    text-decoration: none;
+    color: black;
+    border: 1px solid black;
+    border-radius: 50%;
+    padding: 4px 12px;
+    background-color: white;
+}
 </style>
