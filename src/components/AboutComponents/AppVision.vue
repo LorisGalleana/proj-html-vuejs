79eca43810c7e37cd7ea0a97548892ce18243bfb
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-07-19 11:17:09 +0200

    Fix: Appvision cards hover

--- src/components/AboutComponents/AppVision.vue
+++ src/components/AboutComponents/AppVision.vue
@@ -27,22 +27,22 @@ export default {
             <div class="row my-5 gap-5 flex-nowrap text-white text-center">
                 <div class="col-3">
                     <div class=""><i class="icon fa-solid fa-database" style="color: #ffffff;"></i></div>
-                    <p class="fs-6 fw-semibold">Virtual Document </p>
+                    <p class="fs-4 fw-semibold">Virtual Document </p>
                     <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
                 </div>
                 <div class="col-3">
                     <div><i class="icon fa-solid fa-money-bill-1-wave"></i></div>
-                    <p class="fs-6 fw-semibold">Flexible Payments</p> 
+                    <p class="fs-4 fw-semibold">Flexible Payments</p> 
                     <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
                 </div>
                 <div class="col-3">
                     <div><i class="icon fa-solid fa-gauge-high"></i></div>
-                    <p class="fs-6 fw-semibold">SEO Tools</p>
+                    <p class="fs-4 fw-semibold">SEO Tools</p>
                     <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
                 </div>
                 <div class="col-3">
                     <div><i class="icon fa-regular fa-gem"></i></div>
-                    <p class="fs-6 fw-semibold">UI Design</p>
+                    <p class="fs-4 fw-semibold">UI Design</p>
                     <p class="py-2 mini-font">Far far away, behind the word mountains, far from the countries Vokalia...</p>
                 </div>
             </div>
@@ -59,7 +59,8 @@ export default {
 }
 
 .mini-font {
-    font-size: 0.7rem;
+    font-size: 0.8rem;
+    font-weight: 100;
 }
 
 #white{
@@ -72,8 +73,8 @@ export default {
     padding: 60px 40px;
     border-radius: 10%;
     &:hover {
-        background: $black-gradient;
-        opacity: 0.6;
+        transition: background-color 0.3s;
+        background-color: rgba(0,0,0,0.15);
         cursor: pointer;
     }
 }
@@ -83,7 +84,7 @@ export default {
     display: inline-block;
     border: solid 1px white;
     border-radius: 50%;
-    padding: 1.2rem;
+    padding: 2rem;
     margin-bottom: 20px ;
 }
 
