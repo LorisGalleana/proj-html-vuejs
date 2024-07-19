57969a3f381ebc0e4947713eb74ed346eb104042
Francesco Dente <francesco.dente2003@gmail.com>
2024-07-19 17:34:48 +0200

    fix: header margin

--- src/components/HomeComponents/AppJumbo.vue
+++ src/components/HomeComponents/AppJumbo.vue
@@ -70,7 +70,7 @@
 
 
 <template>
-    <swiper :navigation="true" :loop="true" :modules="modules" class="mySwiper">
+    <swiper :navigation="true" :loop="true" :modules="modules" class="mySwiper mt-5">
         <swiper-slide v-for="(slide, i) in slides" :key="i">
             <div class="container-fluid">
                 <div class="row align-items-center">
